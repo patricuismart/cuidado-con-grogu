ac920d642a90989fcbfa056296109ab8a48df795
patricuismart <patricuismart@gmail.com>
2022-01-30 17:17:06 +0100

    Test counters not accumulate yet

--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -3,22 +3,65 @@ import { NavLink } from 'react-router-dom';
 import { useState, useEffect } from 'react/cjs/react.development';
 
 const GameComponent = () => {
+  //Initial values before rolling  dice
+  const [stateVariable, setStateVariable] = useState([]);
+
+  stateVariable.splice(0, 4);
+  let grogu = 0;
+  let cookies = 3;
+  let frogs = 3;
+  let eggs = 3;
+
   const [min, setMin] = useState(1);
   const [max, setMax] = useState(4);
-  const [number, setNumber] = useState(1);
+  const [number, setNumber] = useState('');
 
   const generateNumber = (min, max) => {
     console.log(`El número aleatorio es ${number}`);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    const math = Math.floor(Math.random() * (max - min + 1) + min);
+    return math;
   };
 
-  const getInputs = () => {
+  const getValues = () => {
+    if (number === 1) {
+      --cookies;
+      if (cookies === 0) {
+        return 'Ya no quedan galletas, sigue tirando!';
+      }
+    } else if (number === 2) {
+      --frogs;
+      if (frogs === 0) {
+        return 'Ya no quedan ranas, sigue tirando!';
+      }
+    } else if (number === 3) {
+      --eggs;
+      if (eggs === 0) {
+        return 'Ya no quedan huevos, sigue tirando!';
+      }
+    } else if (number === 4) {
+      ++grogu;
+      if (grogu === 7) {
+        return 'Grogu ha llegado al armario, fin de la partida!';
+      }
+    }
     setNumber(generateNumber(min, max));
-  };
 
+    console.log(`tengo ${cookies} galletas`);
+    console.log(`tengo ${frogs} ranas`);
+    console.log(`tengo ${eggs} huevos`);
+    console.log(`Grogu ${grogu} avances`);
+  };
+  stateVariable.push(cookies);
+  stateVariable.push(frogs);
+  stateVariable.push(eggs);
+  stateVariable.push(grogu);
   useEffect(() => {
     setNumber(generateNumber(min, max));
   }, []);
+
+  function handleDice() {
+    getValues();
+  }
 
   return (
     <main>
@@ -34,7 +77,7 @@ const GameComponent = () => {
               id="generate"
               type="submit"
               value="Dado"
-              onClick={getInputs}
+              onClick={handleDice}
             />
           </div>
         </div>
