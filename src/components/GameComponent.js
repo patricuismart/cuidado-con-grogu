ff2b086b8ce34738b59d839623e838718dfe674a
patricuismart <patricuismart@gmail.com>
2022-01-30 13:26:46 +0100

    Test game

--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -8,6 +8,17 @@ const GameComponent = () => {
         <button>Home</button>
       </NavLink>
       <h1 className="title">Juego</h1>
+      <input className="dice__btn " type="submit" value="Dado" />
+      <input
+        readOnly
+        type="text"
+        id="clue"
+        className="form__number"
+        value="User Messaje"
+      />
+
+      <p>Número de intentos:;</p>
+      <p>Numbero obtenido</p>
     </main>
   );
 };
