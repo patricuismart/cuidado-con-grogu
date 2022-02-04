0ca8894c5d961e90091bd9594928e19cc851c6ee
patricuismart <patricuismart@gmail.com>
2022-02-04 14:45:57 +0100

    Fix game simple way

--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -5,9 +5,6 @@ import { useState } from 'react/cjs/react.development';
 import yoda from '../images/grogu.png';
 
 const GameComponent = () => {
-  // variable for changing states: cookies, frog, eggs
-
-  const [stateVariable, setStateVariable] = useState([]);
   //Initial values before rolling  dice
   const [gameState, setGameState] = useState({
     grogu: 0,
@@ -16,13 +13,39 @@ const GameComponent = () => {
     eggs: 3,
   });
 
-  // State
   const [number, setNumber] = useState('');
+  const [userMessage, setUserMessage] = useState(false);
 
-  // Math Function radom number
+  // Math Function gettoing radom number
   const generateNumber = (min, max) => {
     const math = Math.floor(Math.random() * (max - min + 1) + min);
     return math;
+  };
+
+  // función reset reload game
+  const handleReset = () => {
+    window.location.reload(true);
+  };
+
+  // GameOver
+
+  const gameOver = () => {
+    handleReset();
+    console.log('Grogu se ha comido todo, Vuelve a intentarlo!');
+    alert('Grogu se ha comido todo, Vuelve a intentarlo!');
+  };
+
+  //Comprobación de si ya tenemos el armario a 0
+  const winner = () => {
+    if (
+      gameState.cookies === 0 &&
+      gameState.frogs === 0 &&
+      gameState.eggs === 0
+    ) {
+      console.log('¡BIEN, Mando completa la misión, Has ganado!');
+      alert('¡BIEN, Mando completa la misión, Has ganado!');
+      handleReset();
+    }
   };
 
   const getValues = () => {
@@ -31,27 +54,32 @@ const GameComponent = () => {
     console.log(`El número aleatorio es ${newNumber}`);
     setNumber(newNumber);
 
+    // Lets play, Game options counter
+    // case 1 cookies
     if (newNumber === 1) {
       if (gameState.cookies === 0) {
         console.log('Ya no quedan galletas, sigue tirando!');
       } else {
         gameState.cookies = gameState.cookies - 1;
       }
+      // case 2 frogs
     } else if (newNumber === 2) {
       if (gameState.frogs === 0) {
         console.log('Ya no quedan ranas, sigue tirando!');
       } else {
         gameState.frogs = gameState.frogs - 1;
       }
+      // case 3 eggs
     } else if (newNumber === 3) {
       if (gameState.eggs === 0) {
         console.log('Ya no quedan huevos, sigue tirando!');
       } else {
         gameState.eggs = gameState.eggs - 1;
       }
+      // case 4 grogu
     } else if (newNumber === 4) {
-      if (gameState.grogu === 7) {
-        console.log('Grogu ha llegado al armario, fin de la partida!');
+      if (gameState.grogu === 6) {
+        gameOver();
       } else {
         gameState.grogu = gameState.grogu + 1;
       }
@@ -68,6 +96,7 @@ const GameComponent = () => {
 
   function handleDice(ev) {
     getValues();
+    winner();
   }
 
   return (
@@ -91,19 +120,22 @@ const GameComponent = () => {
 
       <div>
         <img alt="grogu" title="grogu" className="yoda" src={yoda}></img>
-        {gameState.grogu}
+        <p>Grogu {gameState.grogu} avances</p>
       </div>
       <div>
-        <i className="fas fa-cookie"></i>
-        {gameState.cookies}
+        <p>
+          Te quedan {gameState.cookies} <i className="fas fa-cookie"></i>
+        </p>
       </div>
       <div>
-        <i className="fas fa-frog"></i>
-        {gameState.frogs}
+        <p>
+          Te quedan {gameState.frogs} <i className="fas fa-frog"></i>
+        </p>
       </div>
       <div>
-        <i className="fas fa-egg"></i>
-        {gameState.eggs}
+        <p>
+          Te quedan {gameState.eggs} <i className="fas fa-egg"></i>
+        </p>
       </div>
     </main>
   );
