c86218dd751eb885ad292d9db0cd675c3159b868
patricuismart <patricuismart@gmail.com>
2022-02-06 13:56:54 +0100

    Fix New Layout

--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -20,6 +20,7 @@ const GameComponent = () => {
   // States
   const [number, setNumber] = useState('');
   const [stateWinner, setStateWinner] = useState(false);
+  const [userMessage, setUserMessage] = useState('');
 
   //***FUNCTIONS***//
   // Math Function for random number
@@ -50,6 +51,7 @@ const GameComponent = () => {
     ) {
       console.log('¡BIEN, Mando completa la misión, Has ganado!');
       setStateWinner(true);
+      handleReset();
     }
   };
 
@@ -62,27 +64,34 @@ const GameComponent = () => {
     // Lets play, Game options counter
     // case 1 cookies
     if (newNumber === 1) {
+      setUserMessage('Descargas galleta, sigue tirando!');
       if (gameState.cookies === 0) {
         console.log('Ya no quedan galletas, sigue tirando!');
+        setUserMessage('Ya no quedan galletas, sigue tirando!');
       } else {
         gameState.cookies = gameState.cookies - 1;
       }
       // case 2 frogs
     } else if (newNumber === 2) {
+      setUserMessage('Descargas rana, sigue tirando!');
       if (gameState.frogs === 0) {
         console.log('Ya no quedan ranas, sigue tirando!');
+        setUserMessage('Ya no quedan ranas, sigue tirando!');
       } else {
         gameState.frogs = gameState.frogs - 1;
       }
       // case 3 eggs
     } else if (newNumber === 3) {
+      setUserMessage('Descargas huevo, sigue tirando!');
       if (gameState.eggs === 0) {
         console.log('Ya no quedan huevos, sigue tirando!');
+        setUserMessage('Ya no quedan huevos, sigue tirando!');
       } else {
         gameState.eggs = gameState.eggs - 1;
       }
       // case 4 grogu
     } else if (newNumber === 4) {
+      setUserMessage('Grogu avanza, sigue tirando!');
       if (gameState.grogu === 6) {
         gameOver();
       } else {
@@ -95,6 +104,7 @@ const GameComponent = () => {
     console.log(`Grogu ${gameState.grogu} avances`);
 
     setGameState({ ...gameState });
+    winnerMode();
   };
 
   //Handle function-> on click dice generates random value & test if empty stock of food
@@ -104,49 +114,59 @@ const GameComponent = () => {
   }
   //***RENDER***//
   return (
-    <main className="main">
-      <NavLink to="/">
-        <button className="buton--home">Home</button>
-      </NavLink>
-      <div className="container--game">
-        <div className="container--result">
-          <p className="container--result__paragraf">
-            Resultado dado: {number}
+    <>
+      <section className="board">
+        <input
+          className="result-dice"
+          placeholder="..."
+          type="text"
+          value={number}
+          onChange={handleDice}
+        />
+        <input
+          className="user-message"
+          placeholder="user message"
+          type="text"
+          value={userMessage}
+          onChange={handleDice}
+        />
+        <input
+          className="buton-dice"
+          id="generate"
+          type="button"
+          value="Dado"
+          onClick={handleDice}
+        />
+        <button className="buton-reload" onClick={handleReset}>
+          <i className="fas fa-sync-alt"></i>
+        </button>
+        <img
+          alt="grogu"
+          title="grogu"
+          className="grogu-image"
+          src={grogu}
+        ></img>
+        <div className="piece1"></div>
+        <div className="piece2"></div>
+        <div className="piece3"></div>
+        <div className="piece4"></div>
+        <div className="piece5"></div>
+        <div className="piece6"></div>
+        <p className="grogu-avances">Grogu {gameState.grogu} avances</p>
+        <div className="food-container">
+          <p className="food">
+            {gameState.cookies} <i className="fas fa-cookie icon "></i>
           </p>
-          <input
-            className="buton--dice"
-            id="generate"
-            type="button"
-            value="Dado"
-            onClick={handleDice}
-          />
-          <button className="buton--reload" onClick={handleReset}>
-            Reload
-          </button>
+          <p className="food">
+            {gameState.frogs} <i className="fas fa-frog icon"></i>
+          </p>
+          <p className="food">
+            {gameState.eggs} <i className="fas fa-egg icon"></i>
+          </p>
         </div>
-        <div className="container--grogu">
-          <img
-            alt="grogu"
-            title="grogu"
-            className={gameState.grogu === 0 ? styles.grogu : styles.hidden}
-            src={grogu}
-          ></img>
-          <p className="item--text">Grogu {gameState.grogu} avances</p>
-        </div>
-        <div className="container--yoda__text">
-          <p className="item">
-            {gameState.cookies} <i className="fas fa-cookie"></i>
-          </p>
-          <p className="item">
-            {gameState.frogs} <i className="fas fa-frog"></i>
-          </p>
-          <p className="item">
-            {gameState.eggs} <i className="fas fa-egg"></i>
-          </p>
-        </div>
-      </div>
-      <Winner stateWinner={stateWinner} setStateWinner={setStateWinner} />
-    </main>
+      </section>
+      {/* <Winner stateWinner={stateWinner} setStateWinner={setStateWinner} /> */}
+    </>
   );
 };
 
